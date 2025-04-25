f635ee0b0e27a68f66f981e04994a3daf3c7114c
mahdi39980 <mahdi39980@gmail.com>
2025-04-25 10:32:57 +0600

    Initial commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ const imageRouter = require("./routers/userRouter");
 //
 const app = express();
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'https://99polygon.com',
+    credentials: true,
+  }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
